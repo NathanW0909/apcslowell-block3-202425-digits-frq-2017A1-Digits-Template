c330995d6ec4ac82135dab44df949ba442bcc2fa
NathanW0909 <nawong8@s.sfusd.edu>
2025-04-13 00:41:35 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
 
-public class Digits
-{
+public class Digits {
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	public Digits(int num) {
+		digitList = new ArrayList<>();
+		if (num == 0) {
+			digitList.add(0);
+		} else {
+			for (int n = num; n > 0; n /= 10) {
+				digitList.add(0, n % 10);
+			}
+		}
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	public boolean isStrictlyIncreasing() {
+		for (int i = 1; i < digitList.size(); i++) {
+			if (digitList.get(i) <= digitList.get(i - 1)) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return digitList.toString();
 	}
 }
